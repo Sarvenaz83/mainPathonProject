76fcde1334679afc6ad691ae8f5cd3f68c813350
Sarvenaz Sinaei <sarvenaz.sinaei@student.nbi-handelsakademin.se>
2025-01-19 22:58:39 +0100

    comitt uppgiften 3-1a

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+#1 Skapa projekt och synka med GitHub:
+
 print("Hello World")
 print("This program was made by Sarvenaz Sinaei")
+
+# 3 Använda variabler och datatyper:
+# 1a:
+tal1_str= input("Skriv in ett heltal: ")
+tal1 = int(tal1_str)
+print("Du angav talet:", tal1)
+
+
