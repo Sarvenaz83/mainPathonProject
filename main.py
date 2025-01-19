12490fa7aecd92882feb5b00024b4a2fa30cab78
Sarvenaz Sinaei <sarvenaz.sinaei@student.nbi-handelsakademin.se>
2025-01-19 23:01:43 +0100

    comitt uppgiften 3-1b

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ print("Hello World")
 print("This program was made by Sarvenaz Sinaei")
 
 # 3 Använda variabler och datatyper:
-# 1a:
+# 1a: Be användaren om ett heltal och testa inmatningen
+
 tal1_str= input("Skriv in ett heltal: ")
 tal1 = int(tal1_str)
 print("Du angav talet:", tal1)
+
+# 1b: Be om ytterligare ett heltal och skriv ut summan
+
+tal2_str= input("Skriv in ett annat heltal: ")
+tal2 = int(tal2_str)
+summa =tal1 + tal2
+print("Summan av talen är:",summa)
 
 
